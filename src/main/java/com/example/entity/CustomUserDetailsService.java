1e3790f08c1fdb55fa9ebc550b396c0c0ae36420
Rabiul-Hasan-work <rabiulworkapps@gmail.com>
2021-06-13 15:17:15 +0600

    Update Service Class

--- src/main/java/com/example/entity/CustomUserDetailsService.java
+++ src/main/java/com/example/entity/CustomUserDetailsService.java
@@ -1,9 +1,11 @@
 package com.example.entity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 public class CustomUserDetailsService implements UserDetailsService {
 
@@ -17,6 +19,26 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("User not found");
         }
         return new CustomUserDetails(user);
+    }
+
+    @Autowired UserRepository customerRepo;
+
+    @Autowired
+    JavaMailSender mailSender;
+
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+
+    public void generateOneTimePassword(User customer) {
+
+    }
+
+    public void sendOTPEmail(User customer, String OTP) {
+
+    }
+
+    public void clearOTP(User customer) {
     }
 
 }
